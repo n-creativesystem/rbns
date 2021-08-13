8379020a3d1e11ce8ae55483458b6a58fffb9b6b
ncs-nozomi-nishinohara <nozomi_nishinohara@n-creativesystem.com>
2021-08-13 17:53:30 +0900

    temporary commit

--- handler/restserver/saml.go
+++ handler/restserver/saml.go
@@ -1,1 +1,58 @@
 package restserver
+
+import (
+	"encoding/base64"
+	"encoding/json"
+	"net/http"
+
+	"github.com/crewjam/saml/samlsp"
+	"github.com/gin-gonic/gin"
+	"github.com/n-creativesystem/rbns/internal/saml"
+	"github.com/n-creativesystem/rbns/logger"
+)
+
+const (
+	samlKey = "x-saml-payload"
+)
+
+func samlHandler(r *gin.Engine, opts ...saml.Options) {
+	middleware := saml.New(opts...)
+	handler := func(c *gin.Context) {
+		w := c.Writer
+		r := c.Request
+		middleware.ServeHTTP(w, r)
+	}
+	r.Any("saml/*actions", handler)
+	r.Use(func(c *gin.Context) {
+		w := c.Writer
+		r := c.Request
+		log := logger.FromContext(r.Context())
+		session, err := middleware.Session.GetSession(r)
+		if err != nil {
+			log.Errorf("Session error: %s", err)
+		}
+		if session != nil {
+			buf, err := json.Marshal(session)
+			if err != nil {
+				log.Errorf("Session Json Marshal: %s", err)
+			}
+			strJwt := base64.RawURLEncoding.EncodeToString(buf)
+			c.Request = r.WithContext(samlsp.ContextWithSession(r.Context(), session))
+			w.Header().Add(samlKey, strJwt)
+			c.Set(samlKey, strJwt)
+			c.Next()
+			return
+		}
+		if err == samlsp.ErrNoSession {
+			status := w.Status()
+			middleware.HandleStartAuthFlow(w, r)
+			if w.Status() != status {
+				return
+			}
+			c.Next()
+			return
+		}
+		log.Error(err)
+		c.AbortWithStatus(http.StatusUnauthorized)
+	})
+}
